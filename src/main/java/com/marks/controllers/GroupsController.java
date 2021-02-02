c93c1aa4c71f59ec7511f3cb93ae7bc1bb64c8ca
i.gilmatdinov <i.gilmatdinov@cometrica.ru>
2021-02-03 00:02:19 +0300

    refactoring

--- src/main/java/com/marks/controllers/GroupsController.java
+++ src/main/java/com/marks/controllers/GroupsController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.UUID;
 
-@RestController
+@RestController("/groups")
 public class GroupsController {
     private final GroupService groupService;
 
@@ -18,22 +18,22 @@ public class GroupsController {
         this.groupService = groupService;
     }
 
-    @PostMapping("/addGroup")
+    @PostMapping("/add")
     public List<String> addGroup(@RequestBody String code) {
         return groupService.addGroup(code);
     }
 
-    @PostMapping("/editGroup")
+    @PostMapping("/edit")
     public List<String> editGroup(@RequestBody Group group) {
         return groupService.editGroup(group);
     }
 
-    @GetMapping("/getAllGroups")
+    @GetMapping("/getAll")
     public List<Group> getAllGroups() {
         return groupService.findAllGroups();
     }
 
-    @PostMapping("/deleteGroup")
+    @PostMapping("/delete")
     public List<String> deleteGroup(@RequestBody String uuid) {
         return groupService.deleteGroup(UUID.fromString(uuid));
     }
